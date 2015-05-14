8e8eb550ad2357d4ed2b82eec67807c95335a941
Lucas Thiel <lucasthiel@physics.gmail.com>
2015-05-14 20:21:19 +0200

    Episode 18 Connection Packets

--- CulLight Chat/src/com/CulLight/Chat/Client.java
+++ CulLight Chat/src/com/CulLight/Chat/Client.java
@@ -60,7 +60,7 @@ public class Client extends JFrame {
 		// createWindow is private, so one cant overwrite it.
 		createWindow();
 		console("Attempting a connection to " + address + ":" + port + ", user:" + name);
-		String connection = name + " connected from " + address + ":" + port;
+		String connection = "/c/" + name;
 		send(connection.getBytes());
 	}
 		
